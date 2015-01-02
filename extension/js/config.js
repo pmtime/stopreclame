b49daa496220139bb7e76216d70cdfa2816b7458
Skychko <denysskychko@gmail.com>
2015-01-02 22:59:05 +0200

    change config

--- extension/js/config.js
+++ extension/js/config.js
@@ -1,7 +1,6 @@
 var CONFIG = (function () {
     var _ = LIBRARY,
         module,
-        //domain = "http://127.0.0.1:3000/system";
         domain = "http://stopreclame.com/api";
 
     module = {
